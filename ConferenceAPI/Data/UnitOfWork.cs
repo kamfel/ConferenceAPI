0f2fcdcd90bfed0fa43cb2b80eb6aeca175b0ad8
kamfel <kamil.fel76@gmail.com>
2019-12-17 22:48:44 +0100

    Implement UnitOfWork

--- ConferenceAPI/Data/UnitOfWork.cs
+++ ConferenceAPI/Data/UnitOfWork.cs
@@ -11,25 +11,42 @@ namespace ConferenceAPI.Data
     public class UnitOfWork : IUnitOfWork
     {
         private readonly DbContext _dbContext;
+        private Dictionary<Type, object> _repositories;
 
         public UnitOfWork(DbContext dbContext)
         {
             _dbContext = dbContext;
+            _repositories = new Dictionary<Type, object>();
         }
 
         public void Dispose()
         {
-            throw new NotImplementedException();
+            _dbContext.Dispose();
         }
 
         public IRepository<T> GetRepository<T>() where T : class
         {
-            throw new NotImplementedException();
+            try
+            {
+                _dbContext.Set<T>();
+            }
+            catch (InvalidOperationException)
+            {
+                throw new InvalidOperationException("Entity of type " + typeof(T).ToString() + " doesn't exist in dbContext");
+            }
+
+            if (!_repositories.TryGetValue(typeof(T), out var repository))
+            {
+                repository = new Repository<T>(_dbContext);
+                _repositories.Add(typeof(T), repository);
+            }
+
+            return repository as IRepository<T>;
         }
 
-        public Task<int> SaveChangesAsync()
+        public async Task<int> SaveChangesAsync()
         {
-            throw new NotImplementedException();
+            return await _dbContext.SaveChangesAsync();
         }
     }
 }
