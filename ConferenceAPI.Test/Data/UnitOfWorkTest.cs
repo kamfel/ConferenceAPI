56709ac14872843205cfafec9627b722b6fe9d5c
kamfel <kamil.fel76@gmail.com>
2019-12-17 22:31:22 +0100

    Add UnitOfWork tests

---
+++ ConferenceAPI.Test/Data/UnitOfWorkTest.cs
@@ -0,0 +1,43 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using ConferenceAPI.Core.Models;
+using ConferenceAPI.Data;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace ConferenceAPI.Test.Data
+{
+    public class UnitOfWorkTest
+    {
+        private IFixture _fixture;
+
+        [SetUp]
+        public void SetUp()
+        { 
+            _fixture = new Fixture().Customize(new AutoMoqCustomization());
+            _fixture.Register<DbContextOptions>(() => new DbContextOptions<DbContext>());
+        }
+
+        [Test]
+        public void GetRepository_ValidEntity_Returns()
+        {
+            var mock = _fixture.Freeze<Mock<DbContext>>();
+            mock.Setup(p => p.Set<Room>()).Returns(Mock.Of<DbSet<Room>>());
+            var sut = _fixture.Create<UnitOfWork>();
+
+            Assert.DoesNotThrow(() => sut.GetRepository<Room>(), null);
+        }
+
+        [Test]
+        public void GetRepository_InvalidEntity_Throws()
+        {
+            var mock = _fixture.Freeze<Mock<DbContext>>();
+            mock.Setup(p => p.Set<string>()).Throws(new InvalidOperationException());
+            var sut = _fixture.Create<UnitOfWork>();
+
+            Assert.Throws<InvalidOperationException>(() => sut.GetRepository<string>());
+        }
+    }
+}
