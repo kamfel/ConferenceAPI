facc58a3d604390fb78aec4b9464b7c87ac9cd62
kamfel <kamil.fel76@gmail.com>
2019-12-21 21:21:47 +0100

    Add GET /rooms

--- ConferenceAPI/Controllers/RoomsController.cs
+++ ConferenceAPI/Controllers/RoomsController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using ConferenceAPI.Core;
+using ConferenceAPI.Core.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,10 +15,21 @@ namespace ConferenceAPI.Controllers
     public class RoomsController : ControllerBase
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
 
-        public RoomsController(IUnitOfWork unitOfWork)
+        public RoomsController(IUnitOfWork unitOfWork, IMapper mapper)
         {
             _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        [HttpGet("~/")]
+        public async Task<IActionResult> GetAll()
+        {
+            var rooms = await _unitOfWork.GetRepository<Room>().GetAllAsync();
+            var roomsDTO = _mapper.Map<Room>(rooms);
+
+            return Ok(roomsDTO);
         }
     }
 }
