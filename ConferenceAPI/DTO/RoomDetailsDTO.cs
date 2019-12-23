07e4d9386ccec73a081260305fdc1e47358007ff
kamfel <kamil.fel76@gmail.com>
2019-12-23 21:21:57 +0100

    Add RoomDetails class

---
+++ ConferenceAPI/DTO/RoomDetailsDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConferenceAPI.DTO
+{
+    public class RoomDetailsDTO
+    {
+        public int Number { get; set; }
+        public int Name { get; set; }
+        public string Layout { get; set; }
+        public int NumberOfSeats { get; set; }
+        public IEnumerable<string> Devices { get; set; }
+    }
+}
